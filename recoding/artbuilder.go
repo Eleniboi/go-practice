5ab450d76b3d21b24b2d24616d4c806d55070ff2
somafu <omafuoloche4@gmail.com>
2026-05-30 16:44:24 +0100

    recoding

--- recoding/artbuilder.go
+++ recoding/artbuilder.go
@@ -1,22 +1,35 @@
 package main
 
-import "strings"
+import (
+	"strings"
+)
 
-type textPart struct{ text, style string }
-type ArtBuilder struct{ parts []textPart }
+type textParts struct {
+	text  string
+	style string
+}
 
-func NewArtBuilder() *ArtBuilder { return &ArtBuilder{} }
+type ArtBuilder struct {
+	parts []textParts
+}
+
+func NewArtBuilder() *ArtBuilder {
+	return &ArtBuilder{}
+}
 
 func (b *ArtBuilder) AddText(text string) *ArtBuilder {
-	b.parts = append(b.parts, textPart{text, "normal"})
+
+	b.parts = append(b.parts, textParts{text, "normal"})
 	return b
 }
 
 func (b *ArtBuilder) SetStyle(style string) *ArtBuilder {
-	if style != "normal" && style != "bold" &&
-		style != "italic" && style != "outline" {
+
+	if style != "normal" && style != "italic" && style != "bold" && style != "outline" {
+
 		panic("invalid style")
 	}
+
 	if len(b.parts) > 0 {
 		b.parts[len(b.parts)-1].style = style
 	}
@@ -24,10 +37,12 @@ func (b *ArtBuilder) SetStyle(style string) *ArtBuilder {
 }
 
 func (b *ArtBuilder) Build() string {
+
 	lines := make([]string, 8)
 
-	for _, p := range b.parts {
-		for i, s := range render(p.text, p.style) {
+	for _, pat := range b.parts {
+		for i, s := range render(pat.text, pat.style) {
+
 			lines[i] += s
 		}
 	}
@@ -35,25 +50,34 @@ func (b *ArtBuilder) Build() string {
 }
 
 func render(text, style string) []string {
+
 	lines := make([]string, 8)
 
 	if style == "bold" {
 		text += text
 	}
 	if style == "italic" {
+
 		for i := range lines {
+
 			lines[i] = strings.Repeat(" ", 7-i) + text
 		}
 		return lines
 	}
+
 	if style == "outline" {
+
 		border := "+" + strings.Repeat("-", len(text)) + "+"
+
 		lines[0], lines[7], lines[1] = border, border, "|"+text+"|"
+
 		for i := 2; i < 7; i++ {
+
 			lines[i] = "|" + strings.Repeat(" ", len(text)) + "|"
 		}
 		return lines
 	}
+
 	for i := range lines {
 		lines[i] = text
 	}
