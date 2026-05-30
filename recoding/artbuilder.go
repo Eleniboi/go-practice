c840cac320969c38365871a9cce2ea4ab833282b
somafu <omafuoloche4@gmail.com>
2026-05-30 10:15:16 +0100

    artbuilder

---
+++ recoding/artbuilder.go
@@ -0,0 +1,61 @@
+package main
+
+import "strings"
+
+type textPart struct{ text, style string }
+type ArtBuilder struct{ parts []textPart }
+
+func NewArtBuilder() *ArtBuilder { return &ArtBuilder{} }
+
+func (b *ArtBuilder) AddText(text string) *ArtBuilder {
+	b.parts = append(b.parts, textPart{text, "normal"})
+	return b
+}
+
+func (b *ArtBuilder) SetStyle(style string) *ArtBuilder {
+	if style != "normal" && style != "bold" &&
+		style != "italic" && style != "outline" {
+		panic("invalid style")
+	}
+	if len(b.parts) > 0 {
+		b.parts[len(b.parts)-1].style = style
+	}
+	return b
+}
+
+func (b *ArtBuilder) Build() string {
+	lines := make([]string, 8)
+
+	for _, p := range b.parts {
+		for i, s := range render(p.text, p.style) {
+			lines[i] += s
+		}
+	}
+	return strings.Join(lines, "\n") + "\n"
+}
+
+func render(text, style string) []string {
+	lines := make([]string, 8)
+
+	if style == "bold" {
+		text += text
+	}
+	if style == "italic" {
+		for i := range lines {
+			lines[i] = strings.Repeat(" ", 7-i) + text
+		}
+		return lines
+	}
+	if style == "outline" {
+		border := "+" + strings.Repeat("-", len(text)) + "+"
+		lines[0], lines[7], lines[1] = border, border, "|"+text+"|"
+		for i := 2; i < 7; i++ {
+			lines[i] = "|" + strings.Repeat(" ", len(text)) + "|"
+		}
+		return lines
+	}
+	for i := range lines {
+		lines[i] = text
+	}
+	return lines
+}
