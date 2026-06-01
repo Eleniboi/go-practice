0ccb729fa403506abad3607d78bf2a978f9291c5
somafu <omafuoloche4@gmail.com>
2026-06-01 11:21:58 +0100

    golang

--- recoding.2/builder/animation.go
+++ recoding.2/builder/animation.go
@@ -1,22 +1,46 @@
 package animation
 
-import "strings"
 
-type Animation struct{ frames []string }
+import (
+	"strings"
+)
 
-func NewAnimation(_ string, n int) *Animation {
+type Animation struct {
+	frames []string
+}
+
+
+func NewAnimation(_ string, n int) *Animation{
+
 	return &Animation{make([]string, n)}
 }
 
-func (a *Animation) gen() {
-	for i := range a.frames {
-		ch := string([]rune{'-', '\\', '|', '/'}[i%4])
-		a.frames[i] = strings.Repeat(strings.Repeat(ch, 10)+"\n", 10)
+func (b *Animation) gen() {
+
+	for i := range b.frames{
+		ch := string([]rune{'\\','-','|', '/'}[i%4])
+		b.frames[i] = strings.Repeat(strings.Repeat(ch, 10) + "\n", 10)
 	}
 }
 
-func (a *Animation) GenerateSpinFrames()   { a.gen() }
-func (a *Animation) GenerateWaveFrames()   { a.gen() }
-func (a *Animation) GenerateZoomFrames()   { a.gen() }
-func (a *Animation) GetFrame(i int) string { return a.frames[i%len(a.frames)] }
-func (a *Animation) Play() string          { return "---\n" + strings.Join(a.frames, "---\n") }
+func (b *Animation) GenerateSpinFrames() {
+	b.gen()
+}
+
+func (b *Animation) GenerateWaveFrames() {
+	b.gen()
+}
+
+func (b *Animation) GenerateZoomFrames() {
+	b.gen()
+}
+
+func (b *Animation) GetFrame(i int) string {
+
+	return b.frames[i%len(b.frames)]
+}
+
+func (b *Animation) Play() string {
+
+	return "---\n" + strings.Join(b.frames, "---\n")
+}
