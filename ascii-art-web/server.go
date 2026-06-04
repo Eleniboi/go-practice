fabf05d16d83aef6174e7cecf6090a402ab02694
somafu <omafuoloche4@gmail.com>
2026-06-04 08:40:35 +0100

    restructure

--- ascii-art-web/server.go
+++ ascii-art-web/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"ascii-web/logic"
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
@@ -14,14 +15,14 @@ func asciiHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "This route exists, but it is been access incorrectly")
 		return
 	}
-	
+
 	//the request from the web are been transfer into variables
 	//one for the user input and the other for the banner choice
 
 	text := r.FormValue("text")
 	banner := r.FormValue("banner")
 
-	font, err := logic.LoadBanner(banner)
+	font, err := logic.LoadBanner("banners/" + banner)
 
 	if err != nil {
 		fmt.Fprintln(w, err)
@@ -29,13 +30,26 @@ func asciiHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	result := logic.GenerateArt(text, font)
+	tmpl, err := template.ParseFiles("template/result.html")
 
-	fmt.Fprintln(w, result)
+	if err != nil {
+		return
+	}
+
+	err = tmpl.Execute(w, result)
+
+	if err != nil {
+		return
+	}
+
+	//.fmt.Fprintln(w, result)
+
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Fprintln(w, "template/index.html")
+	fmt.Fprintln(w, "<html><head></head><body><h1>how are you today</h1></body></html>")
+
 }
 
 func main() {
